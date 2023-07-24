729a30ee1366c2241fdce273b2c6756fff241458
Agustin Lanusse <aglanuss@cbr15s3.42barcelona.com>
2023-07-24 12:09:01 +0200

    final commit

---
+++ ex00/rush03.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush03.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ede-la-c <ede-la-c@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/09 16:19:10 by ede-la-c          #+#    #+#             */
+/*   Updated: 2023/07/09 16:20:05 by ede-la-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_putchar.h"
+
+void	ft_lines(int *pos, int x, int y, char *p)
+{
+	int	a;
+	int	b;
+
+	a = pos[0];
+	b = pos[1];
+	if (b > 0 && b < y - 1 && a > 0 && a < x - 1)
+	{
+		ft_putchar(' ');
+	}
+	else if (a > 0 && a < x - 1)
+	{
+		ft_putchar(p[4]);
+	}
+	else if (b > 0 && b < y - 1)
+	{
+		ft_putchar(p[5]);
+	}
+}
+
+void	ft_graph(int *pos, int x, int y, char *p)
+{
+	int	a;
+	int	b;
+
+	a = pos[0];
+	b = pos[1];
+	if (b == 0 && a == 0)
+	{
+		ft_putchar(p[0]);
+	}
+	else if (a == x - 1 && a > 0 && b == 0)
+	{
+		ft_putchar(p[1]);
+	}
+	else if (b == y - 1 && b > 0 && a == 0)
+	{
+		ft_putchar(p[2]);
+	}
+	else if (b == y - 1 && a == x - 1)
+	{
+		ft_putchar(p[3]);
+	}
+	else
+	{
+		ft_lines(pos, x, y, p);
+	}
+}
+
+void	rush(int x, int y)
+{
+	int		pos[2];
+	char	p[6];
+
+	p[0] = 'A';
+	p[1] = 'C';
+	p[2] = 'A';
+	p[3] = 'C';
+	p[4] = 'B';
+	p[5] = 'B';
+	pos[0] = 0;
+	pos[1] = 1;
+	pos[1] = 0;
+	while (pos[1] < y)
+	{
+		pos[0] = 0;
+		while (pos[0] < x)
+		{
+			ft_graph(pos, x, y, p);
+			pos[0]++;
+		}
+		ft_putchar('\n');
+		pos[1]++;
+	}
+}
